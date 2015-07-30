0f42f86863577c6805762bb81697fa09fbba6ae6
dennisledford <dledford@gmail.com>
2015-07-30 07:44:12 -0400

    added hashmap for comicVine first appearnce api result

--- herolyze/src/main/java/com/dledford/herolyze/domain/ComicVineCharacter.java
+++ herolyze/src/main/java/com/dledford/herolyze/domain/ComicVineCharacter.java
@@ -4,6 +4,8 @@
 package com.dledford.herolyze.domain;
 
 
+import java.util.Map;
+
 import com.dledford.herolyze.utils.CharacterClassType;
 /**
  * @author dledford
@@ -14,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ComicVineCharacter extends BaseEntity{
 	private String name;
+	private Map<String,String> first_appeared_in_issue;
 	private String firstAppearance;
 	private CharacterClassType characterClass;
 
@@ -23,17 +26,23 @@ public class ComicVineCharacter extends BaseEntity{
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getFirstAppearance() {
-		return firstAppearance;
+	public Map<String,String> getFirst_appeared_in_issue() {
+		return first_appeared_in_issue;
 	}
-	public void setFirstAppearance(String firstAppearance) {
-		this.firstAppearance = firstAppearance;
+	public void setFirst_appeared_in_issue(Map<String,String> first_appeared_in_issue) {
+		this.first_appeared_in_issue = first_appeared_in_issue;
 	}
 	public CharacterClassType getCharacterClass() {
 		return characterClass;
 	}
 	public void setCharacterClass(CharacterClassType characterClass) {
 		this.characterClass = characterClass;
+	}
+	public String getFirstAppearance() {
+		return firstAppearance;
+	}
+	public void setFirstAppearance(String firstAppearance) {
+		this.firstAppearance = firstAppearance;
 	}
 	
 }
